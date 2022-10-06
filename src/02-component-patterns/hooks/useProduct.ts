cc812d8f06abc28285daf3de0048224c40a60fbb
Max <maxdoleo@gmail.com>
2022-10-06 12:25:34 -0400

    Control props alternativo terminado

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,37 +1,30 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { onChangeArgs, Product } from '../interfaces/interfaces';
 
 
 interface useProductArgs {
     product: Product;
-    onChange?: (args: onChangeArgs) => void;
+    onChange?: ( args: onChangeArgs ) => void;
     value?: number;
 }
 
+
 export const useProduct = ({ onChange, product, value = 0 }: useProductArgs) => {
 
-    const [counter, setCounter] = useState(value);
+    const [ counter, setCounter ] = useState( value );
 
-    const isControlled = useRef(!!onChange);
-    
 
-    const increaseBy = (value: number) => {
-
-        if (isControlled.current) {
-            return onChange!({ count: value, product });
-        }
-
-        const newValue = Math.max(counter + value, 0)
-
-        setCounter(newValue);
+    const increaseBy = ( value: number ) => {
+      
+        const newValue = Math.max( counter + value, 0 )
+        setCounter( newValue );
 
         onChange && onChange({ count: newValue, product });
     }
 
     useEffect(() => {
-     setCounter(value);
-    }, [value])
-    
+        setCounter( value );
+    }, [ value ])
 
     return {
         counter,
